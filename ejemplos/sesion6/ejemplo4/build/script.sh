55bca17f5b84d8a2f33ab0dd966cdbebd8391eff
José Domingo Muñoz <josedom24@gmail.com>
2021-03-25 22:21:02 +0100

    ejemplo4

--- ejemplos/sesion6/ejemplo4/build/script.sh
+++ ejemplos/sesion6/ejemplo4/build/script.sh
@@ -2,7 +2,7 @@
 sed -i 's/$this->user="root";/$this->user="'${MARIADB_USER}'";/g' /var/www/html/core/controller/Database.php
 sed -i 's/$this->pass="";/$this->pass="'${MARIADB_PASS}'";/g' /var/www/html/core/controller/Database.php
 sed -i 's/$this->host="localhost";/$this->host="'${MARIADB_HOST}'";/g' /var/www/html/core/controller/Database.php
-sleep 3
+sleep 5
 mysql -u ${MARIADB_USER} -p${MARIADB_PASS} -h ${MARIADB_HOST} bookmedik < /opt/schema.sql
 
 apache2ctl -D FOREGROUND
